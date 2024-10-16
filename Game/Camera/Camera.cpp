788a1a0e31edecee8a9bd616c49d57011681c52f
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-10-16 23:38:38 +0900

    add comment

--- Game/Camera/Camera.cpp
+++ Game/Camera/Camera.cpp
@@ -45,13 +45,14 @@ void Camera::Update(){
 
 			if(pPlayer_->GetTransform()->GetTranslation().y > 10.0f){
 
+				// 高さの割合を計算
  				float heightRatio = std::clamp((pPlayer_->GetTransform()->GetTranslation().y - 10.0f) / 40.0f, 0.0f, 1.0f);
 				
-				// translate
+				// 飛行中のプレイヤーの高さに応じた追加の調整ベクトルを計算
 				addVec.y = 7.0f * (pPlayer_->GetTransform()->GetTranslation().y - 10.0f) / 40.0f;
 				addVec.x = 10.0f * heightRatio;
 
-				// rotate
+				// 飛行中の定位置まで回転を加算
 				transform_.rotate.x += 0.005f * GameTimer::TimeRate();
 				transform_.rotate.x = std::clamp(transform_.rotate.x, baseRotate.x, baseRotate.x + 0.15f);
 				transform_.rotate.y += 0.005f * GameTimer::TimeRate();
@@ -60,6 +61,8 @@ void Camera::Update(){
 			}
 
 		} else{
+
+			// 回転を徐々にもとに戻す
 			transform_.rotate.x -= 0.005f * GameTimer::TimeRate();
 			transform_.rotate.x = std::clamp(transform_.rotate.x, baseRotate.x, baseRotate.x + 0.15f);
 			transform_.rotate.y -= 0.005f * GameTimer::TimeRate();
