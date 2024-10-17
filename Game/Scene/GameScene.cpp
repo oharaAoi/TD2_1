1215540b1f1015a0ca3c6efb0c26f3b0f68a6bc5
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-10-17 10:44:15 +0900

    marge

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -83,6 +83,7 @@ void GameScene::Init(){
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 void GameScene::Load(){
+	ModelManager::LoadModel("./Game/Resources/Model/Player/", "Player.fbx");
 	ModelManager::LoadModel("./Game/Resources/Model/Player/", "Player_Head.obj");
 	ModelManager::LoadModel("./Game/Resources/Model/Player/", "Player_Torso.obj");
 	ModelManager::LoadModel("./Game/Resources/Model/Player/", "Player_Tail.obj");
