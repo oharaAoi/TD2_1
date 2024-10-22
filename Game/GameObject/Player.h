45a0952cb0ee63ceea4d6a3fed6fc9e3a16bef54
testName <k023g0068@g.neec.ac.jp>
2024-10-23 04:09:50 +0900

    着水効果音が2回再生されるバグを修正

--- Game/GameObject/Player.h
+++ Game/GameObject/Player.h
@@ -156,11 +156,16 @@ private:
 	float dontInputPressTime_;
 	float dontInputTime_ =0.6f;
 
-	float seCoolTime;
 
 	// 滑空開始直後の猶予時間
 	const float kAllowableTime = 1.0f;
 	float collisionAllowableTime_ = 1.0f;
+
+	//移動切り替え効果音のクールタイム
+	float seCoolTime;
+
+	//着水効果音の猶予時間
+	float waterSurfaceCoolTime;
 
 	// フラグ-------------------------------------------
 
