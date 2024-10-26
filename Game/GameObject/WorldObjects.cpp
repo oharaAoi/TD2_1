06be84b6831fdb7d7af35223926b09300fbfd2dc
oharaAoi <k023g0108@g.neec.ac.jp>
2024-10-26 17:13:40 +0900

    worldObjectの変数の初期化忘れを追加

--- Game/GameObject/WorldObjects.cpp
+++ Game/GameObject/WorldObjects.cpp
@@ -16,6 +16,12 @@ WorldObjects::~WorldObjects() {
 void WorldObjects::Init() {
 	cloudZ = 130.0f;
 
+	stageLoopCount_ = 0;
+	nowStageIndex_ = 0;
+
+	mountainLoopCount_ = 0;
+	nowMountainIndex_ = 0;
+
 	for (uint32_t oi = 0; oi < kStageMax_; ++oi) {
 		Vector3 newPos = StageInformation::worldWallPos_;
 		newPos.x += StageInformation::stageWidthEvery_ * (oi);
