c1eb0f7207f969e68c7445da9d77125825fa7427
oharaAoi <k023g0108@g.neec.ac.jp>
2025-07-03 15:42:33 +0900

    bodyとspeedの文字の間隔を調整

--- Game/UI/PlayerSpeedCounter.h
+++ Game/UI/PlayerSpeedCounter.h
@@ -134,9 +134,9 @@ private:
 
 	Vector2 announcePos_;
 	Vector2 speedLocalPos_ = {-100.0f, 0.0f};
-	Vector2 percentLocalPos_ = { 125.0f, 0.0f };
-	Vector2 numberLocalPos_;
-	Vector2 numberSpriteDivision_ = {64.0f, 0.0f};
+	Vector2 percentLocalPos_ = { 150.0f, 0.0f };
+	Vector2 numberLocalPos_ = { 24.0f, 0.0f };
+	Vector2 numberSpriteDivision_ = { 55.0f, 0.0f};
 
 	float drawHeight = 80.0f;//速度のアナウンスの描画する高さ
 
